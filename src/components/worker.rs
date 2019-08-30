162a540763f309569c6a75aed376e3cd8852a705
Blaine Price <1wbprice@gmail.com>
2019-08-30 14:55:23 -0400

    rough out worker stuff

--- src/components/worker.rs
+++ src/components/worker.rs
@@ -1,3 +1,20 @@
+use amethyst::ecs::prelude::{
+    Component,
+    DenseVecStorage
+}
+
+enum Task {
+    TAKING_ORDER,
+    CLEANING,
+    COOKING
+}
+
 struct Worker {
-    satisfaction: u8
+    satisfaction: u8,
+    velocity: f[f32; 2],
+    task: Task
+}
+
+impl Component for Worker {
+    type Storage = DenseVecStorage<Self>;
 }
