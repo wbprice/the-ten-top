f37f1ec05fd0e4500cf657958fb1a756a3901481
Blaine Price <1wbprice@gmail.com>
2019-09-07 01:17:48 -0400

    begin to randomize patron placemnet (#12)

--- src/components/patron.rs
+++ src/components/patron.rs
@@ -1,11 +1,29 @@
 use amethyst::ecs::prelude::{Component, DenseVecStorage};
-
-use crate::components::Food;
+use rand::Rng;
 
 #[derive(Debug)]
 pub struct Patron {
     pub satisfaction: u8,
     pub velocity: [f32; 2],
+    pub origin: [f32; 2]
+}
+
+impl Patron {
+    pub fn generate() -> Patron {
+        let mut rng = rand::thread_rng();
+
+        Patron {
+            satisfaction: rng.gen_range(0, 8),
+            velocity: [
+                rng.gen_range(15.0, 25.0),
+                0.0
+            ],
+            origin: [
+                -10.0,
+                rng.gen_range(0.0, 48.0)
+            ]
+        }
+    }
 }
 
 impl Component for Patron {
