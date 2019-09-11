b29792c8e3639b47b1741750329bfd4b2e4f7fa9
Blaine Price <1wbprice@gmail.com>
2019-09-11 11:12:19 -0400

    Don't fight the borrow checker

--- src/systems/destination.rs
+++ src/systems/destination.rs
@@ -1,7 +1,7 @@
 use amethyst::{
     core::timing::Time,
     core::transform::{Parent, Transform},
-    ecs::prelude::{Join, Read, ReadStorage, System, WriteStorage},
+    ecs::prelude::{Join, Read, ReadStorage, System, WriteStorage, Entities, Entity},
 };
 
 use crate::components::{
@@ -13,15 +13,35 @@ pub struct DestinationSystem;
 
 impl<'s> System<'s> for DestinationSystem {
     type SystemData = (
+        Entities<'s>,
         WriteStorage<'s, Patron>,
         ReadStorage<'s, Transform>,
-        ReadStorage<'s, Destination>
+        WriteStorage<'s, Destination>
     );
 
-    fn run(&mut self, (mut patrons, locals, destinations): Self::SystemData) {
-        for (patron, patron_local, destination) in (&patrons, &locals, &destinations).join() {
-            dbg!(patron);
-            dbg!(destination);
+    fn run(&mut self, (entities, mut patrons, locals, mut destinations): Self::SystemData) {
+
+        let mut destinations_to_remove : Vec<Entity> = vec![];
+
+        for (entity, patron, patron_local, destination) in (&entities, &patrons, &locals, &destinations).join() {
+            // Did patron arrive at their destination?
+            let pos = patron_local.translation();
+            if pos.x.floor() == destination.x.floor() &&
+               pos.y.floor() == destination.y.floor() {
+               // If so, remove the destination and zero out velocity.
+               destinations_to_remove.push(entity.clone());
+            } else {
+                dbg!(patron);
+                // If not, are they going the right way?
+
+                // If not, change their velocity
+
+            }
+        }
+
+        // Clean up entities
+        for entity in destinations_to_remove {
+            destinations.remove(entity);
         }
     }
 }
