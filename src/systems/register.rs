f369c55da4f21a6a919827f68343569d41dd9d36
Blaine Price <1wbprice@gmail.com>
2019-09-10 22:21:26 -0400

    Rough out register system for attracting patrons.

--- src/systems/register.rs
+++ src/systems/register.rs
@@ -19,12 +19,21 @@ impl<'s> System<'s> for RegisterSystem {
         Read<'s, Time>
     );
 
-    fn run(&mut self, (patrons, registers, locals, time): Self::SystemData) {
+    fn run(&mut self, (mut patrons, registers, locals, time): Self::SystemData) {
         // For each register
         for (register, register_local) in (&registers, &locals).join() {
-            // Check to see if a patron is in range
-            for (patron, patron_local) in (&patrons, &locals).join() {
-                dbg!(patron);
+            // Check to see if a patron is in range by looking at x value of register...
+            let register_x = register_local.translation().x;
+
+            for (patron, patron_local) in (&mut patrons, &locals).join() {
+                // and comparing it to the x of each patron
+                let patron_x = patron_local.translation().x;
+
+                // If there's a match, attract the patron
+                if register_x.floor() == patron_x.floor() {
+                    dbg!("should attract patron");
+                    dbg!(patron);
+                }
             }
         }
     }
