a88f616e86d420bc68a89dd4b65ef2161e7ac1bf
Blaine Price <1wbprice@gmail.com>
2019-09-19 18:52:29 -0400

    implement simple animations for workers

--- src/systems/worker.rs
+++ src/systems/worker.rs
@@ -5,7 +5,7 @@ use amethyst::{
     renderer::SpriteRender,
 };
 
-use crate::components::{SimpleAnimation, Velocity, Worker};
+use crate::components::{Direction, SimpleAnimation, Velocity, Worker};
 
 pub struct WorkerSystem;
 
@@ -27,6 +27,62 @@ impl<'s> System<'s> for WorkerSystem {
         for (entity, _, velocity, local) in (&entities, &workers, &velocities, &mut locals).join() {
             local.prepend_translation_x(velocity.x * time.delta_seconds());
             local.prepend_translation_y(velocity.y * time.delta_seconds());
+
+            // If velocity is zeroed out, remove the animation.
+            if velocity.x == 0.0 && velocity.y == 0.0 {
+                animations.remove(entity);
+                let mut sprite = sprites.get_mut(entity).unwrap();
+                sprite.sprite_number = 0;
+            } else {
+                // Fetch the animation so we can decide if we need
+                // to set it or not.
+                match animations.get(entity) {
+                    Some(animation) => {
+                        match velocity.get_direction() {
+                            Direction::Up => {
+                                // TODO: Update with walking up sprites
+                                if animation.start_sprite_index != 1 {
+                                    println!("Change to walking up");
+                                    animations
+                                        .insert(entity, SimpleAnimation::new(1, 6, 0.1))
+                                        .unwrap();
+                                }
+                            }
+                            Direction::Down => {
+                                // TODO: Update with walking down sprites
+                                if animation.start_sprite_index != 1 {
+                                    println!("Change to walking down");
+                                    animations
+                                        .insert(entity, SimpleAnimation::new(1, 6, 0.1))
+                                        .unwrap();
+                                }
+                            }
+                            Direction::Left => {
+                                // TODO: Update with walking left sprites
+                                if animation.start_sprite_index != 1 {
+                                    println!("Change to walking left");
+                                    animations
+                                        .insert(entity, SimpleAnimation::new(1, 6, 0.1))
+                                        .unwrap();
+                                }
+                            }
+                            _ => {
+                                if animation.start_sprite_index != 1 {
+                                    println!("Change to walking right");
+                                    animations
+                                        .insert(entity, SimpleAnimation::new(1, 6, 0.1))
+                                        .unwrap();
+                                }
+                            }
+                        }
+                    }
+                    None => {
+                        animations
+                            .insert(entity, SimpleAnimation::new(1, 6, 0.1))
+                            .unwrap();
+                    }
+                }
+            }
         }
     }
 }
