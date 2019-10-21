2aa844ba733a8cba0f8ce6f78c58c9700c46d2b4
Blaine Price <blaine.price@cloudreach.com>
2019-10-21 16:37:49 -0400

    Hot dog is being created, just at the wrong location.

--- src/systems/food.rs
+++ src/systems/food.rs
@@ -1,26 +1,50 @@
 use amethyst::{
-    ecs::prelude::{Join, ReadStorage, System, WriteStorage},
+    ecs::prelude::{Join, ReadStorage, Entities, Entity, ReadExpect, System, WriteStorage},
     renderer::SpriteRender,
 };
 
-use crate::components::{Foods, Food};
+use crate::{
+    components::{Food, Foods},
+    resources::{SpriteResource}
+};
 
 pub struct FoodSystem;
 
 impl<'s> System<'s> for FoodSystem {
-    type SystemData = (ReadStorage<'s, Food>, WriteStorage<'s, SpriteRender>);
+    type SystemData = (
+        Entities<'s>,
+        ReadStorage<'s, Food>,
+        WriteStorage<'s, SpriteRender>,
+        ReadExpect<'s, SpriteResource>
+    );
 
-    fn run(&mut self, (foods, mut sprites): Self::SystemData) {
-        for (food, sprite) in (&foods, &mut sprites).join() {
-            // What kind of food is this?
+    fn run(&mut self, (entities, foods, mut sprites, sprite_resource): Self::SystemData) {
+        // One time food setup
+        // Identity new foods that need sprites
+
+        let mut sprites_to_insert : Vec<(Entity, SpriteRender)> = vec![];
+
+        for (entity, food, _) in (&entities, &foods, !&sprites).join() {
+            let sprite_sheet = sprite_resource.sprite_sheet.clone();
+
             match food.food {
                 Foods::HotDog => {
-                    sprite.sprite_number = 20;
+                    sprites_to_insert.push((entity, SpriteRender {
+                        sprite_sheet,
+                        sprite_number: 20,
+                    }));
                 }
                 _ => {
-                    sprite.sprite_number = 17;
+                    sprites_to_insert.push((entity, SpriteRender {
+                        sprite_sheet,
+                        sprite_number: 17,
+                    }));
                 }
             }
+        }
+
+        for (entity, sprite) in sprites_to_insert {
+            sprites.insert(entity, sprite).unwrap();
         }
     }
 }
--- src/systems/plate.rs
+++ src/systems/plate.rs
@@ -1,51 +1,72 @@
 use amethyst::{
     core::timing::Time,
     core::transform::{Parent, Transform},
-    ecs::prelude::{Entities, Entity, Join, ReadStorage, Read, System, WriteStorage},
-    renderer::SpriteRender
+    ecs::prelude::{Entities, Entity, Join, Read, ReadStorage, System, WriteStorage},
+    renderer::SpriteRender,
 };
 
-use crate::{
-    components::{
-        Plate,
-        Ingredient,
-        Ingredients
-    }
-};
+use crate::components::{Food, Foods, Ingredient, Ingredients, Plate};
 
 pub struct PlateSystem;
 
 impl<'s> System<'s> for PlateSystem {
     type SystemData = (
         Entities<'s>,
-        ReadStorage<'s, Parent>,
+        WriteStorage<'s, Parent>,
         ReadStorage<'s, Plate>,
-        WriteStorage<'s, Ingredient>
+        WriteStorage<'s, Ingredient>,
+        WriteStorage<'s, Food>,
+        WriteStorage<'s, Transform>,
     );
 
-    fn run(&mut self, (entities, parents, plates, ingredients): Self::SystemData) {
+    fn run(
+        &mut self,
+        (entities, mut parents, plates, mut ingredients, mut foods, mut locals): Self::SystemData,
+    ) {
+        let mut ingredients_to_remove: Vec<Entity> = vec![];
+        let mut foods_to_create: Vec<(Entity, Foods)> = vec![];
+
         for (plate_entity, plate) in (&entities, &plates).join() {
             // Iterate through the plates.
             // What ingredients have this plate as a parent?
-            let ingredients_and_entity : Vec<(Entity, &Parent, &Ingredient)> = (&entities, &parents, &ingredients)
-                .join()
-                .filter(|(_, parent, _)| parent.entity == plate_entity)
-                .collect();
+            let ingredients_and_entity: Vec<(Entity, &Parent, &Ingredient)> =
+                (&entities, &parents, &ingredients)
+                    .join()
+                    .filter(|(_, parent, _)| parent.entity == plate_entity)
+                    .collect();
 
-            let ingredient_types : Vec<Ingredients> = ingredients_and_entity
+            let ingredient_types: Vec<Ingredients> = ingredients_and_entity
                 .iter()
                 .map(|(_, _, ingredient)| ingredient.ingredient)
                 .collect();
 
             // Do they make a hot dog?
-            let hot_dog_ingredients: Vec<Ingredients> = vec![
-                Ingredients::HotDogWeiner,
-                Ingredients::HotDogBun,
-            ];
+            let hot_dog_ingredients: Vec<Ingredients> =
+                vec![Ingredients::HotDogWeiner, Ingredients::HotDogBun];
 
-            if hot_dog_ingredients.into_iter().all(|ingred| ingredient_types.contains(&ingred)) {
+            if hot_dog_ingredients
+                .into_iter()
+                .all(|ingred| ingredient_types.contains(&ingred))
+            {
+                dbg!("Remove the ingredients");
                 dbg!("Make a food component here!");
+                for (entity, _, _) in ingredients_and_entity {
+                    ingredients_to_remove.push(entity);
+                }
+                foods_to_create.push((plate_entity, Foods::HotDog));
             }
+        }
+
+        for entity in ingredients_to_remove {
+            ingredients.remove(entity).unwrap();
+        }
+        for (parent, food) in foods_to_create {
+            entities
+                .build_entity()
+                .with(Transform::default(), &mut locals)
+                .with(Parent { entity: parent }, &mut parents)
+                .with(Food { food: food }, &mut foods)
+                .build();
         }
     }
 }
