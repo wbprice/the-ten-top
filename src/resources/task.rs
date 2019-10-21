f83aea5cb3f0fef992903ceee21a0d9ec2964acc
Blaine Price <blaine.price@cloudreach.com>
2019-10-21 13:10:02 -0400

    Add plate ingredient task

--- src/resources/task.rs
+++ src/resources/task.rs
@@ -8,6 +8,7 @@ pub enum Tasks {
     DeliverOrder { patron: Entity, food: Foods },
     FetchIngredient { ingredient: Ingredients },
     PrepIngredient { ingredient: Ingredients },
+    PlateIngredient { ingredient, Ingredients, plate: Entity},
     PrepOrder { food: Foods },
     GiveOrder { register: Entity, food: Foods },
 }
